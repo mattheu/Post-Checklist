74aa4dae3688bc00a227a9fa5f4568c3b10616ef
Matth_eu <matthew@matth.eu>
2015-02-19 18:33:34 +0000

    Allow registering of new tests

--- js/post-checklist.js
+++ js/post-checklist.js
@@ -2,7 +2,8 @@
 
 	window.fusionPostChecklist = window.fusionPostChecklist || {};
 
-	var self = window.fusionPostChecklist;
+	var self = this;
+	var post = window.fusionPostChecklist.post;
 
 	self.models      = {};
 	self.collections = {};
@@ -11,37 +12,43 @@
 	/**
 	 * Single Test Model.
 	 *
-	 * Models should have a label, a test callback function and a test status.
-	 * Test status will get updated when the state of the global post model changes.
+	 * Models should have a label, a test callback function and a status.
+	 * Item status will get updated with the result of the test
+	 * when the state of the global post model changes.
 	 */
-	self.models.test = Backbone.Model.extend({
+	self.models.item = Backbone.Model.extend({
 
 		defaults: {
 			label:  '',
-			testCallback: null,
+			test: null,
 			status: 'unknown', // expects bool or string.
 		},
 
 		initialize: function() {
 			this.updateStatus();
-			self.post.bind( 'change', this.updateStatus, this );
+			post.bind( 'change', this.updateStatus, this );
 		},
 
 		updateStatus: function() {
-			var status = this.get( 'testCallback' )( self.post );
-			this.set( 'status', status );
+
+			var testFunc = window[ this.get('test') ];
+			if ( typeof testFunc === 'function' ) {
+    			var status = testFunc( post );
+    			this.set( 'status', status );
+			}
+
 		}
 
 	});
 
-	self.collections.tests = Backbone.Collection.extend({
-		model: self.models.test,
+	self.collections.items = Backbone.Collection.extend({
+		model: self.models.item,
 	})
 
 	self.models.checklist = Backbone.Model.extend({
 		defaults: {
-			title: 'Post Publish Checklist',
-			tests: self.collections.tests,
+			title: fusionPostChecklistData.strings.title,
+			items: self.collections.items,
 		},
 	});
 
@@ -51,22 +58,22 @@
 		className: 'fpc-list',
 
 		initialize: function() {
-			this.model.get('tests').bind( "change reset add remove", this.render, this);
+			this.model.get('items').bind( "change reset add remove", this.render, this);
 		},
 
 		render: function() {
 
 			var t = this;
 
-			if ( this.model.get('tests').length < 1 ) {
+			if ( this.model.get('items').length < 1 ) {
 				this.$el.html('');
 				return this;
 			}
 
 			this.$el.html( this.template( this.model.toJSON() ) );
 
-			this.model.get('tests').each( function( test ) {
-				t.views.add( 'ul', new self.views.item( { model: test } ) );
+			this.model.get('items').each( function( item ) {
+				t.views.add( 'ul', new self.views.item( { model: item } ) );
 			} );
 
 			return this;
@@ -109,38 +116,16 @@
 
 	$(document).ready( function() {
 
-		var tests = new self.collections.tests([
-			{
-				label: 'Has at least 3 narratives.',
-				testCallback: function( post ) {
-					var count = 0;
-					var terms = post.get('terms');
-					for ( var tax in terms ) {
-						count += terms[tax].length;
-					}
-					return count > 3;
-				}
-			},
-			{
-				label: 'Content between 500 and 1500 words.',
-				testCallback: function( post ) {
-					var content = post.get('content') || '';
-					var length  = content.split(' ').length;
-					return ( length > 500 && length < 1500 ) ? true : false;
-				}
-			},
-			{
-				label: 'Has post thumbnail',
-				testCallback: function( post ) {
-					return post.get('featured_image') ? true : false;
-				}
-			},
-		]);
+		console.log( fusionPostChecklistData );
 
-		var checklistModel = new self.models.checklist( { tests: tests } );
+		var checklistItems = new self.collections.items( fusionPostChecklistData.items );
+
+		var checklistModel = new self.models.checklist( { items: checklistItems } );
 		var checklistView  = new self.views.checklist( { model: checklistModel } );
 
 		checklistView.render().$el.prependTo( $( '#major-publishing-actions' ) );
+
+		window.fusionPostChecklist.view = checklistView;
 
 	} );
 
--- post-checklist.php
+++ post-checklist.php
@@ -9,27 +9,111 @@ add_action( 'init', function() {
 	$ver = '0.1';
 
 	wp_register_script( 'wp-api-js', plugins_url( 'wp-api-client-js/wp-api.js', __FILE__ ), array( 'jquery', 'underscore', 'backbone' ), $ver, true );
-	wp_register_script( 'fusion-post-checklist-model', plugins_url( 'js/post-model.js', __FILE__ ), array( 'wp-api-js' ), $ver, true );
-	wp_register_script( 'fusion-post-checklist', plugins_url( 'js/post-checklist.js', __FILE__ ), array( 'fusion-post-checklist-model' ), $ver, true );
+	wp_register_script( 'fpc-model', plugins_url( 'js/post-model.js', __FILE__ ), array( 'wp-api-js' ), $ver, true );
+	wp_register_script( 'fpc', plugins_url( 'js/post-checklist.js', __FILE__ ), array( 'fpc-model' ), $ver, true );
 
-	wp_register_style( 'fusion-post-checklist', plugins_url( 'css/post-checklist.css', __FILE__ ), array(), $ver );
+	wp_register_style( 'fpc', plugins_url( 'css/post-checklist.css', __FILE__ ), array(), $ver );
 
 	wp_localize_script( 'wp-api-js', 'WP_API_Settings', array(
 		'root' => null,
 		'nonce' => wp_create_nonce( 'wp_json' ),
 	) );
 
-	wp_localize_script( 'fusion-post-check', 'WP_API_Settings', array(
-		'strings' => array(
-			'title' => __( 'Post Publish Checklist', 'fusion-post-checklist' ),
-		),
-	) );
 
-	wp_enqueue_script( 'fusion-post-checklist' );
-	wp_enqueue_style( 'fusion-post-checklist' );
+	/**
+	 * Register items using the fpc_items filter.
+	 * Required args
+	 * - label - item description.
+	 * - test - a JS function that will be called
+	 */
+	$items = array_map(
+		function( $args ) {
+			return wp_parse_args( $args, array(
+				'label' => '',
+				'test'  => '',
+			) );
+		},
+		apply_filters( 'fpc_items', array() )
+	);
+
+	wp_localize_script(
+		'fpc',
+		'fusionPostChecklistData',
+		array(
+			'items' => $items,
+			'strings' => array(
+				'title' => __( 'Post Publish Checklist', 'fpc' ),
+			),
+		)
+	);
+
+	wp_enqueue_script( 'fpc' );
+	wp_enqueue_style( 'fpc' );
 
 	add_action( 'admin_footer', function() {
+		do_action( 'fpc_tests' );
 		include( dirname( __FILE__ ) . '/templates/templates.php' );
 	} );
 
-} );
+}, 100 );
+
+/**
+ * Register checklist items.
+ */
+add_filter( 'fpc_items', function( $items ) {
+
+	$items[] = array(
+		'label' => 'Has at least 3 narratives.',
+		'test'  => 'fpcTestNarratives',
+	);
+
+	$items[] = array(
+		'label' => 'Has post featured iamge.',
+		'test'  => 'fpcTestFeaturedImage',
+	);
+
+	$items[] = array(
+		'label' => 'Content between 500 and 1500 words.',
+		'test'  => 'fpcTestContentLength',
+	);
+
+	return $items;
+
+});
+
+/**
+ * Output the FPC tests.
+ */
+add_action( 'fpc_tests', function() {
+
+	?>
+
+<script type="text/javascript">
+
+	var fpcTestNarratives = function( post ) {
+
+		var count = 0;
+		var terms = post.get('terms');
+
+		for ( var tax in terms ) {
+			count += terms[tax].length;
+		}
+
+		return count > 3;
+
+	}
+
+	var fpcTestFeaturedImage = function( post ) {
+		return post.get('featured_image') ? true : false;
+	}
+
+	var fpcTestContentLength = function( post ) {
+		var content = post.get('content') || '';
+		var length  = content.split(' ').length;
+		return ( length > 500 && length < 1500 ) ? true : false;
+	}
+
+</script>
+
+	<?php
+});
